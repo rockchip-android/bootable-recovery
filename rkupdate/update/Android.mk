697ea9347400a7ce235cdad1da4f225aa086482a
jkand.huang <jkand.huang@rock-chips.com>
2017-01-12 10:32:18 +0800

    librkupdate.a:support for rk3399.
    Change-Id: I52e63c524f7638f879475538510e7fb37081571a
    Signed-off-by: jkand.huang <jkand.huang@rock-chips.com>

--- rkupdate/update/Android.mk
+++ rkupdate/update/Android.mk
@@ -2,9 +2,13 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 ifeq ($(TARGET_ARCH), arm64)
-	LOCAL_PREBUILT_LIBS := lib64/librkupdate.a
+  ifeq ($(TARGET_BOARD_PLATFORM), rk3399)
+    LOCAL_PREBUILT_LIBS := lib64/vendor/librkupdate.a
+  else
+    LOCAL_PREBUILT_LIBS := lib64/librkupdate.a
+endif
 else ifeq ($(TARGET_ARCH), arm)
-	LOCAL_PREBUILT_LIBS := lib/librkupdate.a
+    LOCAL_PREBUILT_LIBS := lib/librkupdate.a
 endif
 
 include $(BUILD_MULTI_PREBUILT)
