4ad30bce36bfe1988431b707a1e0c096b7e9b3bb
jkand.huang <jkand.huang@rock-chips.com>
2017-01-17 14:37:27 +0800

    librkupdate.a:support for rk3328.
    Change-Id: I28b6d18277d82f51b81fa734eb6fabc9ae67d1fa
    Signed-off-by: jkand.huang <jkand.huang@rock-chips.com>

--- rkupdate/update/Android.mk
+++ rkupdate/update/Android.mk
@@ -4,9 +4,11 @@ include $(CLEAR_VARS)
 ifeq ($(TARGET_ARCH), arm64)
   ifeq ($(TARGET_BOARD_PLATFORM), rk3399)
     LOCAL_PREBUILT_LIBS := lib64/vendor/librkupdate.a
+  else ifeq ($(TARGET_BOARD_PLATFORM), rk3328)
+    LOCAL_PREBUILT_LIBS := lib64/vendor/librkupdate.a
   else
     LOCAL_PREBUILT_LIBS := lib64/librkupdate.a
-endif
+  endif
 else ifeq ($(TARGET_ARCH), arm)
     LOCAL_PREBUILT_LIBS := lib/librkupdate.a
 endif
