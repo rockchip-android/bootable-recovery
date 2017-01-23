ddc7a36ee2b18cf93aac1666f0c1d25fa954f712
jkand.huang <jkand.huang@rock-chips.com>
2017-01-23 09:25:41 +0800

    librkupdate.a: first to read /dev/vendor_storage, if error, next read /dev/rknand_sys_storage.
    Change-Id: I70d1ceca94cc9ad1f2fb5f07676fd9938eeb84c8
    Signed-off-by: jkand.huang <jkand.huang@rock-chips.com>

--- rkupdate/update/Android.mk
+++ rkupdate/update/Android.mk
@@ -2,13 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 ifeq ($(TARGET_ARCH), arm64)
-  ifeq ($(TARGET_BOARD_PLATFORM), rk3399)
-    LOCAL_PREBUILT_LIBS := lib64/vendor/librkupdate.a
-  else ifeq ($(TARGET_BOARD_PLATFORM), rk3328)
-    LOCAL_PREBUILT_LIBS := lib64/vendor/librkupdate.a
-  else
     LOCAL_PREBUILT_LIBS := lib64/librkupdate.a
-  endif
 else ifeq ($(TARGET_ARCH), arm)
     LOCAL_PREBUILT_LIBS := lib/librkupdate.a
 endif
