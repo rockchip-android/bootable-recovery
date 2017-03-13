33cb984dfa34d6d4d342c18b8e64507ad3a67ba0
jkand.huang <jkand.huang@rock-chips.com>
2017-03-13 16:29:55 +0800

    rk3399: support sdboot update.
    Change-Id: Idfc74b3657b053c42ce723593336421d1950fd66
    Signed-off-by: jkand.huang <jkand.huang@rock-chips.com>

--- rkupdate/update/Android.mk
+++ rkupdate/update/Android.mk
@@ -2,7 +2,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 ifeq ($(TARGET_ARCH), arm64)
+  ifeq ($(TARGET_BOARD_PLATFORM), rk3399)
+    LOCAL_PREBUILT_LIBS := lib64/rk3399/librkupdate.a
+  else
     LOCAL_PREBUILT_LIBS := lib64/librkupdate.a
+  endif
 else ifeq ($(TARGET_ARCH), arm)
     LOCAL_PREBUILT_LIBS := lib/librkupdate.a
 endif
